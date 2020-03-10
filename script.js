a0e7ee1b8c5a9d05b1f6b6955a83d378b63f6f64
JaysonD <jaduff20@gmail.com>
2020-03-10 18:24:38 -0400

    began question functions, functionality not yet in place

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ var timeCount = document.getElementById("timer");
 var gameOverScreen = document.getElementById("game-over");
 var resetGame = document.getElementById("reset");
 var welcomeScreen = document.getElementById("welcome");
+var totalScore = document.getElementById("total-score");
 
 //---BUTTON FUNCTIONALITY---//
 startBtn.addEventListener("click", startGame);
@@ -27,16 +28,20 @@ ans2.addEventListener("click", checkAnswer);
 ans3.addEventListener("click", checkAnswer);
 ans4.addEventListener("click", checkAnswer);
 resetGame.addEventListener("click", startGame);
+a = "";
+b = "";
+c = "";
+d = "";
 
+//---TIMER AND SCORING---//
+var timeLeft = 10;
 
 //---GAME FUNCTIONS---//
 function startGame() {
   welcomeScreen.classList.add("collapse");
-  questionBox.classList.remove("collapse");
   timeCount.classList.remove("collapse");
   gameOverScreen.classList.add("collapse");
 
-  var timeLeft = 10;
 
   var timeInterval = setInterval(function() {
     timeCount.textContent = "Time: " + timeLeft + " seconds remaining";
@@ -48,6 +53,19 @@ function startGame() {
       gameOver();
     }
   }, 1000);
+  chooseAnswer();
+  question1();
+}
+
+function chooseAnswer() {
+  questionBox.classList.remove("collapse");
+  correctAnswer.classList.add("collapse");
+  wrongAnswer.classList.add("collapse");
+
+  // ans1
+  // ans2
+  // ans3
+  // ans4
 }
 
 function checkAnswer() {
@@ -55,10 +73,32 @@ function checkAnswer() {
     correctAnswer.classList.remove("collapse");
   } else {
     wrongAnswer.classList.remove("collapse");
+    timeLeft = timeLeft - 10;
   }
 }
 
 function gameOver() {
-    questionBox.classList.add("collapse");
-    gameOverScreen.classList.remove("collapse");
+  questionBox.classList.add("collapse");
+  gameOverScreen.classList.remove("collapse");
+  totalScore.textContent = timeLeft + 1;
+}
+
+//---QUESTIONS---//
+
+function question1() {
+  questionContent.textContent =
+    "Which of these is not a keyword in Javascript?";
+  ans1.textContent = "var";
+  ans2.textContent = "function";
+  ans3.textContent = "for";
+  ans4.textContent = "banana";
+
+  if (event.ans4) {
+    correctAnswer.classList.remove("collapse");
+    chooseAnswer();
+  } else {
+    wrongAnswer.classList.remove("collapse");
+    timeLeft - 10;
+    chooseAnswer();
+  }
 }
